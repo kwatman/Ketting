decbfbc9e9002fdec1152c4daadeae6a3ec5150a
dolfijn3000 <yente.paternotte@hotmail.com>
2022-10-19 13:28:58 +0200

    added keypair

---
+++ Ketting/KeyPair.cs
@@ -0,0 +1,26 @@
+﻿using System.Security.Cryptography;
+
+namespace Ketting;
+
+public class KeyPair
+{
+    public RSACryptoServiceProvider rsa { get; set; }
+    public KeyPair()
+    {
+        rsa = new RSACryptoServiceProvider(2048);
+        RSAParameters publicKey = rsa.ExportParameters(false);
+        RSAParameters privateKey = rsa.ExportParameters(true);
+    }
+
+    public Byte[] Sign(Byte[] data)
+    {
+        return rsa.SignData(data,SHA256.Create());
+    }
+
+    public static bool Verify(RSAParameters key, byte[] dataToVerify,byte[] originalData)
+    {
+        RSACryptoServiceProvider rsaVerify = new RSACryptoServiceProvider();
+        rsaVerify.ImportParameters(key);
+        return rsaVerify.VerifyData(originalData,SHA256.Create() ,dataToVerify);
+    }
+}
