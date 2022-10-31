8fa76df0e41e75414ca04eddd7e53f3119d93036
Thomas Goris <thomas.goris1@student.ucll.be>
2022-10-31 11:29:24 +0100

    Write to filles.

---
+++ Ketting/WriteToXml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace Ketting
+{
+    public class WriteToXml
+    {
+        public void WriteXml(string path, string fileName, Object @object )
+        {
+            try
+            {
+                if (!File.Exists(Path.Combine(path, fileName)))
+                {
+                    Console.WriteLine("true"); // halen we er later uit
+                    XmlSerializer serializer = new XmlSerializer(typeof(Block));
+                    FileStream stream = File.OpenWrite(Path.Combine(path, fileName));
+                    serializer.Serialize(stream, @object );
+                    stream.Dispose();
+                }
+                else
+                {
+                    Console.WriteLine("false"); // halen we er later uit
+                    XmlSerializer serializer = new XmlSerializer(typeof(Block));
+                    StreamWriter stream = File.AppendText(Path.Combine(path, fileName));
+                    serializer.Serialize(stream, @object );
+                    stream.Dispose();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+                // schrijf naar een log als we dit doen. 
+            }
+        }
+        
+        public void ReadXml(string path, string fileName)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(Block));
+            FileStream streamRead = File.OpenRead(path + fileName);
+            var result = (Block)(serializer.Deserialize(streamRead));
+        }
+    }
+}
