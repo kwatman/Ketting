4b1ce1190db81dd39cda3a782e0454def5667276
Yessie1601 <94546645+Yessie1601@users.noreply.github.com>
2022-11-01 16:58:00 +0100

    npm server werkt

---
+++ Front-end/Public/Js/index.js
@@ -0,0 +1,20 @@
+import Wallet from "./wallet.js"; 
+import Block from "./block.js";
+import WalletInfo from "./walletInfo.js";
+
+
+Wallet = {
+    mywallet: document.querySelector("#mywallet"),
+    searchWallet: document.querySelector("#searchWalletButton"),
+}
+// wallet.mywallet.addEventListener("click", console.log(mywallet));
+
+Block ={
+    searchBlock: document.querySelector("#searchBlockButton"),
+}
+
+WalletInfo = {
+    
+}
+
+
---
+++ Front-end/Public/Js/wallet.js
@@ -0,0 +1,16 @@
+export default class Wallet{
+
+    constructor(publicKey,balance){
+        this._publicKey = publicKey;
+        this._balance = balance;
+    }
+
+    get publicKey(){return this._publicKey;}
+    get balance(){return this._balance;}
+
+    toHtmlString(){
+        return `
+            <li class="list-group-item" id="">${this.publicKey}" "${this.balance}</li>
+        `
+    }
+}
