64a247e06fde8c541c934cae46d47780c054a184
Yessie1601 <94546645+Yessie1601@users.noreply.github.com>
2022-11-03 12:51:27 +0100

    fetch made

--- Front-end/Public/Js/index.js
+++ Front-end/Public/Js/index.js
@@ -3,31 +3,65 @@ import Block from "./block.js";
 import WalletInfo from "./walletInfo.js";
 
 const loginBtn = document.getElementById("btnLogin");
+let count = 0;
+const completeTranscaction = document.getElementById("completeTranscaction")
+const searchBlock = document.querySelector("#searchBlockButton")
+const mywallet =document.querySelector("#mywallet")
+const searchWallet = document.querySelector("#searchWalletButton")
 
-loginBtn.addEventListener("click", (event) => {
-    const inputFieldLogin = document.getElementById("loginPrivateKey").value;
-
-    if(inputFieldLogin.toString().length == 0) {
-        alert("Please enter a private key");
+if(loginBtn != null){
+loginBtn.addEventListener("click", async(e) => {
+    e.preventDefault();
+    const loginPrivate = document.getElementById("loginPrivateKey").value;
+    const loginPublic = document.getElementById("loginPublicKey").value;
+    if(loginPrivate.toString().length == 0 || loginPublic.toString().length == 0) {
+        alert("Please enter a private key and public key");
     }else{
-        localStorage.setItem("loginKey", inputFieldLogin);  
-        console.log(localStorage.getItem("loginKey")); 
-
+        localStorage.setItem("privateKey", loginPrivate); 
+        localStorage.setItem("publicKey", loginPublic);  
+        console.log("Private Key: " + localStorage.getItem("privateKey") + "// Public Key: " + localStorage.getItem("publicKey")); 
     }  
-});
+})};
 
-const wallet ={
-    mywallet: document.querySelector("#mywallet"),
-    searchWallet: document.querySelector("#searchWalletButton"),
-}
-// wallet.mywallet.addEventListener("click", console.log(mywallet));
 
-const block ={
-    searchBlock: document.querySelector("#searchBlockButton"),
-}
+if(completeTranscaction != null){
+    completeTranscaction.addEventListener("click", async(e)=>{
+        e.preventDefault();
+        count++
+        const amount= document.getElementById("amount").value;
+        const receiver = document.getElementById("receiver").value;
+        const datum = Date.now();
+        const loginPrivate = localStorage.getItem("privateKey");
+        const loginPublic = localStorage.getItem("publicKey");
+        const string="SgLYNAIOlfMOSYOn1nojnD4XesO1SX26MUi/XOJm8tG9zZfZ5HxhjoM7s2PgIkQW0BmU/JIhrvQWf5Ul1g5hQnvWC/LavHso3XgjrZ7937xyzD+mSPq0f93YaIurrYq+FJPCRVKLy2XQ7673+4PWj41Td2jxKBNUw9Z7GcW3jlnoqYP6BXaiwwlU8f2BRBPxfK8LDrJhy5W9BDq/SqIZcnjNkqFch5xYrNUNPRzkbql9LP7pSDjO9L1LiKrRBAOy8CDUuCNgwLMoEKyGccdw22GDBb3nP+SU/f4fsI7qJTndgHCQP0qTg0Y7W8dPBJHGr+ZdxRTLpzSKgLW75JSR0w=="
+        let signNotEncrypt = localStorage.getItem("publicKey") + "@" + receiver + "@" + amount + "@" + datum;
 
-const walletInfo = {
+        if(amount.toString().length >0 && receiver.toString().length > 0){
+            
+        // var sign = new JSEncrypt();
+        // sign.setPrivateKey(loginPrivate.val());
+        // var signature = sign.sign(signNotEncrypt.val(), CryptoJS.SHA256, "sha256");
+
+        let transaction = {
+            "transactionNumber" : count,
+            "amount" : amount,
+            "timeStamp" : "2022-11-03T11:18:00",
+            "senderKey" : loginPublic,
+            "receiverKey" : receiver,
+            "signature" : string,
+            "type" : 0
+        }
+        await fetch("http://localhost:7262/transaction",{
+            method: "POST",
+            header: {"Content-Type":"application/json"},
+            body: JSON.stringify(transaction)   
+        }).then(async(res) => { 
+                alert("Transaction of " + amount + " has been completed");
+        });
+        }else{alert("Fill the inputfields to make transaction");}
     
-}
+        console.log(amount)// verwijdern als het werkt
+})};
+
 
 
--- Front-end/Server/server.js
+++ Front-end/Server/server.js
@@ -1,57 +1,12 @@
 import express from "express";
+import cors from "cors";
 
 const port = 3000;
 const app = express();
 
+app.use(cors());
 app.use(express.static('../Public'));
 app.use(express.json());
-
-//GET Requests
-app.get("/blockchain" , (req, res) => {
-    let blockchain = searchBlocks(
-        req.query.prevHash,
-        req.query.version,
-        req.query.data,
-        req.query.timestamp,
-        req.query.publicKey,
-        req.query.hash,
-        req.query.signature);
-});
-
-app.get("/keypair" , (req, res)=>{
-    let keypair = searchKeypair(
-        req.query.privateKey,
-        req.query.publicKey
-    );
-});
-
-// app.get(`/wallet/${publicKey}/balance`, (req, res) => {
-
-// });
-
-// app.get(`/wallet/${publicKey}` , (req, res) =>{
-//     let wallet = searchWallet(
-//         req.query.address,
-//         req.query.balance,
-//         req.query.transaction
-//     );
-// });
-
-//POST Requests
-app.post("/transaction" , (req, res) => {
-
-    let transaction = makeTransaction(
-        req.query.transactionNumber,
-        req.query.amount,
-        req.query.timestamp,
-        req.query.senderKey,
-        req.query.receiverKey,
-        req.query.signature
-    );
-    res.json({
-        transaction: transaction
-    });
-});
 
 app.listen(port, function() {
     console.log('listening on port ' + port);
