4eb10437512b372fe1907ecc7220e30abbbb5b23
Yente Paternotte <yente.paternotte@hotmail.com>
2022-11-07 10:30:00 +0100

    added docker

--- Front-end/server.js
+++ Front-end/server.js
@@ -5,7 +5,7 @@ const port = 3000;
 const app = express();
 
 app.use(cors());
-app.use(express.static('../Public'));
+app.use(express.static('./Public'));
 app.use(express.json());
 
 app.listen(port, function() {
